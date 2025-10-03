2d141a7b25ad61daa0304766bb143c548dd0c408
Your Name <federickgrendalo@gmail.com>
2025-10-03 23:53:03 +0300

    fixed error

--- client/src/routes/AuthRoute.jsx
+++ client/src/routes/AuthRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../auth/AuthContext";
 export default function AuthRoute({ children }) {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, loading } = useAuth();
   if (isAuthenticated) {
     return <Navigate to="/dashboard" replace />;
   } else if (!loading) return children;
--- client/src/routes/ProtectedRoute.jsx
+++ client/src/routes/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../auth/AuthContext";
 export default function ProtectedRoute({ children }) {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, loading } = useAuth();
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
   } else if (!loading) return children;
